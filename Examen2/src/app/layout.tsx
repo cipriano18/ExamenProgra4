478654f7d5734710342887f55b9fe40946b8d06a
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2025-06-14 14:39:40 -0600

    configuracion para pwa

--- Examen2/src/app/layout.tsx
+++ Examen2/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { montserrat } from './ui/fonts';
 import './ui/global.css';
+import Head from 'next/head'; // Importar Head
 
 export default function RootLayout({
   children,
@@ -8,8 +9,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </Head>
       <body className={`${montserrat.className} antialiased`}>
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
