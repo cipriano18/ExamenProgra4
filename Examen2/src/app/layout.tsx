68da88e387683733634cc542c9cf71c17e49d61b
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2025-06-14 19:47:50 -0600

    arreglo instalacion

--- Examen2/src/app/layout.tsx
+++ Examen2/src/app/layout.tsx
@@ -4,9 +4,11 @@ import { montserrat } from './ui/fonts';
 export const metadata = {
   title: 'FlowTasks',
   description: 'Tu centro de control para gestionar tareas con eficiencia y estilo.',
+  themeColor: '#0f172a',
   manifest: '/manifest.json',
   icons: {
-    apple: '/icon.png',
+    icon: '/icons/icon-192x192.png',
+    apple: '/icons/icon-192x192.png',
   },
   appleWebApp: {
     capable: true,
