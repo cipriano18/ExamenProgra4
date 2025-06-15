d24073b50f6aaf4aea2c7275fb9782772c0e83c7
ReynerRojasG <reyner.rojas.gutierrez@est.una.ac.cr>
2025-06-14 19:31:18 -0600

    update-pwa

--- Examen2/src/app/layout.tsx
+++ Examen2/src/app/layout.tsx
@@ -4,11 +4,9 @@ import { montserrat } from './ui/fonts';
 export const metadata = {
   title: 'FlowTasks',
   description: 'Tu centro de control para gestionar tareas con eficiencia y estilo.',
-  themeColor: '#0f172a',
   manifest: '/manifest.json',
   icons: {
-    icon: '/icons/icon-192x192.png',
-    apple: '/icons/icon-192x192.png',
+    apple: '/icon.png',
   },
   appleWebApp: {
     capable: true,
